ce2ab6e3140182c974e70ef333ee33fe3d7959a2
leon <leonjerineck@gmail.com>
2025-02-03 15:40:58 +0100

    fuck linear regression

--- Linear_regression/train.py
+++ Linear_regression/train.py
@@ -6,7 +6,7 @@
 #    By: ljerinec <ljerinec@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/01/31 14:27:25 by ljerinec          #+#    #+#              #
-#    Updated: 2025/02/01 18:16:17 by ljerinec         ###   ########.fr        #
+#    Updated: 2025/02/03 15:34:49 by ljerinec         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -19,7 +19,6 @@ def main() -> None:
     data = read_dataset()
     if not data.empty:
         linear_regression(data)
-        display_graph(data)
 
 
 def read_dataset() -> pd.DataFrame:
@@ -32,50 +31,90 @@ def read_dataset() -> pd.DataFrame:
         return (None)
 
 
-def display_graph(data: pd.DataFrame) -> None:
+def display_graph(data: pd.DataFrame, Theta0, Theta1) -> None:
     if not data.empty:
         data.plot.scatter(x="km", y="price")
-        plt.axline((0, 0), slope=0, color='green')
-        plt.savefig("Plot repartition")
-        # plt.show()
+        plt.axline((0, Theta0), slope=Theta1, color='green')
+        # plt.savefig("Plot repartition")
+        plt.show()
 
 
 def linear_regression(data):
-    Theta0 = 0.5
+    Theta0 = 0
     Theta1 = 0
-
+    learn_rate = 0.1
     price = np.array(data['price'])
     mileage = np.array(data['km'])
-    standt_price = (price - price.mean()) / price.std()
-    standt_mileage = (mileage - mileage.mean()) / mileage.std()
-    print("Standardized price :\n", standt_price, "\n")
-    print("Standardized mileage :\n", standt_mileage, "\n")
+    norm_price = (price - np.min(price)) / (np.max(price) - np.min(price))
+    norm_mileage = (mileage - np.min(mileage)) / (np.max(mileage) - np.min(mileage))
+    print("Normalized price :\n", norm_price, "\n")
+    print("Normalized mileage :\n", norm_mileage, "\n")
 
-    predicted_price = Theta0 + (Theta1 * standt_mileage)
-    print("Predicted price for each mileage of the dataset:\n", predicted_price, "\n")
-    # predicted_price.rename(columns={"km": "price"}, inplace=True)
-    # print(predicted_price)
-    # print(standt_data["price"])
-    # print(predicted_price.subtract(standt_data["price"]))
-    # mse = 1 / predicted_price.size() * (predicted_price - price).pow(2)
-    mse = np.array(predicted_price - standt_price)
-    mse = np.power(mse, 2)
-    print("Result of pow:\n", np.around(mse, 2), "\n")
-    sum_mse = np.sum(mse)
-    real_sum = 0
-    for i in mse:
-        real_sum += i
-    print("Result of real_sum:\n", real_sum, "\n")
-    print("Result of sum:\n", sum_mse, "\n")
-    print("MSE size:\n", mse.size, "\n")
-    res_mse = 1 / mse.size * sum_mse
-    print("Res MSE size:\n", res_mse, "\n")
-    
-    # print(predicted_price.shape[0])
-        # new_t0 = 
-        # new_t1 = 
+    for i in range(0, 1):
+        predicted_price = Theta0 + (Theta1 * mileage)
+        print("Predicted price for each mileage of the dataset:\n", predicted_price, "\n")
+        # mse = np.array(predicted_price - norm_price)
+        # mse = np.power(mse, 2)
+        # sum_mse = np.sum(mse)
+        # res_mse = 1 / mse.size * sum_mse
+        # print("Result of MSE :\n", res_mse, "\n")
 
+        # test = np.sum(predicted_price - norm_price)
+        # print("Sum of norm values ", test)
+
+        new_t0 = learn_rate * (1 / price.size) * np.sum(predicted_price - price)
+        print(np.sum(predicted_price - price))
+        new_t1 = learn_rate * (1 / price.size) * np.sum((predicted_price - price) * mileage)
+        print(np.sum((predicted_price - price) * mileage))
+        Theta0 = new_t0
+        Theta1 = new_t1
+        print("Theta0 =", Theta0)
+        print("Theta1 =", Theta1)
+        display_graph(data, Theta0, Theta1)
     # print(standt_data)
+
+
+def undo_normalisation(value, dataset):
+    return value * ((np.max(dataset) - np.min(dataset)) + np.min(dataset))
+
+
+
+
+
+
+
+# standt_price = (price - price.mean()) / price.std()
+    # standt_mileage = (mileage - mileage.mean()) / mileage.std()
+    # print("Standardized price :\n", standt_price, "\n")
+    # print("Standardized mileage :\n", standt_mileage, "\n")
+
+    # for i in range(0, 20):
+        # predicted_price = Theta0 + (Theta1 * standt_mileage)
+        # print("Predicted price for each mileage of the dataset:\n", predicted_price, "\n")
+        # mse = np.array(predicted_price - standt_price)
+        # mse = np.power(mse, 2)
+        # sum_mse = np.sum(mse)
+        # res_mse = 1 / mse.size * sum_mse
+        # print("Result of MSE :\n", res_mse, "\n")
+
+        # test = np.sum(np.round(predicted_price, 4) - np.round(standt_price, 4))
+        # print("test ", test)
+
+        # new_t0 = learn_rate * (1 / mse.size) * np.sum(predicted_price - standt_price)
+        # print("\n", learn_rate, (1 / mse.size), np.sum(predicted_price - standt_price))
+        # print(new_t0)
+        # new_t1 = learn_rate * (1 / mse.size) * np.sum((predicted_price - standt_price) * standt_mileage)
+        # print(new_t1)
+        # Theta0 = new_t0
+        # Theta1 = new_t1
+        # print("Theta0 =", Theta0)
+        # print("Theta1 =", Theta1)
+        # display_graph(data, Theta0 * price.std() + price.mean(), Theta1 * mileage.std() + mileage.mean())
+    # print(standt_data)
+
+
+
+
 
 
 if __name__ == "__main__":
