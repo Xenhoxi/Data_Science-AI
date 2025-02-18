bf45b1782b1b0981b4d66223bce96043dbd7fbb3
leon <leonjerineck@gmail.com>
2025-02-18 17:22:37 +0100

    dslr read_dataset

--- dslr/data_analysis/describe.py
+++ dslr/data_analysis/describe.py
@@ -6,25 +6,33 @@
 #    By: ljerinec <ljerinec@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/02/18 15:37:57 by ljerinec          #+#    #+#              #
-#    Updated: 2025/02/18 16:00:51 by ljerinec         ###   ########.fr        #
+#    Updated: 2025/02/18 17:22:10 by ljerinec         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 import sys
+import pandas as pd
 
 
 def main():
     try:
         assert len(sys.argv) == 2, "Wrong numbers of arguments"
-        check_path(sys.argv[1])
+        Dataset = read_dataset(sys.argv[1])
+        if Dataset is not None:
+            print("Dataset read successfully")
     except (AssertionError) as err:
-        print(err)
+        print(f"AssertionError: {err}")
     except (KeyboardInterrupt):
         pass
 
 
-def check_path():
-    pass
+def read_dataset(path: str) -> pd.DataFrame:
+    try:
+        data = pd.read_csv(path)
+        return (data)
+    except FileNotFoundError:
+        print("Unexpected error, impossible to read the dataset !")
+        return (None)
 
 
 if __name__ == "__main__":
