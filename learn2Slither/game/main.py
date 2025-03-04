c75795c5fdaa29e2f57413459ad51dff5a19dd8f
leon <leonjerineck@gmail.com>
2025-03-04 16:52:47 +0100

    snake OK, no apples

--- learn2Slither/game/main.py
+++ learn2Slither/game/main.py
@@ -5,7 +5,7 @@ WIDTH, HEIGHT = 500,500
 ARRAY_WIDTH = 10
 SQUARE_WIDTH = WIDTH / ARRAY_WIDTH
 
-WHITE = (220,220,220)
+WHITE = (200,200,200)
 BLACK = (10,10,10)
 BLUE = (0,0,230)
 
@@ -29,8 +29,14 @@ def draw_background(screen):
 
 def main():
     screen, running, clock = init_game()
+    frame = 0
+    game_on = False
+
     #init()
     snake = Snake(screen, SQUARE_WIDTH, BLUE)
+    screen.fill(WHITE)
+    draw_background(screen)
+    snake.draw()
 
     while running:
         keys = pygame.key.get_pressed()
@@ -41,16 +47,27 @@ def main():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     running = False
+                if event.key == pygame.K_SPACE:
+                    game_on = True
                 snake.change_move(event.key)
                 snake.print_grid(event.key)
 
-        #update()
-        snake.move()
+        if game_on:
+            #update()
+            if frame > 20:
+                snake.move()
+                snake.print_grid(pygame.K_i)
+                frame = 0
 
-        #draw()
-        screen.fill(WHITE)
-        draw_background(screen)
-        frame = snake.draw()
+            if snake.is_dead():
+                snake = Snake(screen, SQUARE_WIDTH, BLUE)
+                game_on = False
+
+            #draw()
+            screen.fill(WHITE)
+            draw_background(screen)
+            snake.draw()
+            frame += 1
 
         clock.tick(60)
         pygame.display.update()
--- learn2Slither/game/snake.py
+++ learn2Slither/game/snake.py
@@ -1,18 +1,43 @@
 import pygame
 import numpy as np
 import random
+import os
+import time
 
 
 class Snake:
-	def __init__(self, screen, width = 50, color=(0,0,210), grid_size=10):
-		self.x = random.randint(0, 9)
-		self.y = random.randint(0, 9)
+	def __init__(self, screen, width = 50, color=(100,0,100), grid_size=10):
+		self.x = random.randint(2, 7)
+		self.y = random.randint(2, 7)
 		self.dir = self.random_dir()
+		self.grid_size = grid_size
 		self.grid = np.zeros((grid_size, grid_size))
 		self.grid[self.x][self.y] = 1
+		self.set_snake_on_grid(self.x, self.y)
+		self.set_apple_on_grid()
 		self.width = width
 		self.color = color
 		self.screen = screen
+		self.img_head = pygame.image.load(os.path.join("game/snake_head.png"))
+
+	
+	def set_apple_on_grid(self):
+		pass
+
+
+	def set_snake_on_grid(self, x, y):
+		if self.dir == 'UP':
+			self.grid[x + 1, y] = 2
+			self.grid[x + 2, y] = 3
+		if self.dir == 'DOWN':
+			self.grid[x - 1, y] = 2
+			self.grid[x - 2, y] = 3
+		if self.dir == 'LEFT':
+			self.grid[x, y + 1] = 2
+			self.grid[x, y + 2] = 3
+		if self.dir == 'RIGHT':
+			self.grid[x, y - 1] = 2
+			self.grid[x, y - 2] = 3
 
 
 	def random_dir(self):
@@ -28,32 +53,74 @@ class Snake:
 
 
 	def draw(self):
-		pygame.draw.rect(self.screen, self.color,
-				   (self.x * self.width, self.y * self.width, self.width, self.width))
+		for x in range(self.grid_size):
+			for y in range(self.grid_size):
+				if self.grid[y, x] == 1:
+					self.screen.blit(self.img_head, (x * self.width, y * self.width))
+				elif self.grid[y, x] > 1:
+					pygame.draw.rect(self.screen, self.color,
+							   (x * self.width, y * self.width, self.width, self.width))
 		
+
+	def find_head(self):
+		for x in range(self.grid_size):
+			for y in range(self.grid_size):
+				if self.grid[y, x] == 1:
+					return x, y
+				
+
+	def find_tail(self):
+		max = self.grid.max()
+		for x in range(self.grid_size):
+			for y in range(self.grid_size):
+				if self.grid[y, x] == max:
+					return x, y
+
+
+	def add_1_to_grid(self):
+		for x in range(self.grid_size):
+			for y in range(self.grid_size):
+				if self.grid[y, x] >= 1:
+					self.grid[y, x] += 1
+
 
 	def move(self):
-		# if self.dir:
-			# x, y = find_head()
-			# x, y = find_tail()
+		if not self.dir:
+			return
+		y, x = self.find_head()
+		self.add_1_to_grid()
 		if self.dir == 'UP':
-			pass
+			if x - 1 >= 0:
+				self.grid[x - 1, y] = 1
 		if self.dir == 'DOWN':
-			pass
+			if x + 1 < self.grid_size:
+				self.grid[x + 1, y] = 1
 		if self.dir == 'LEFT':
-			pass
+			if y - 1 >= 0:
+				self.grid[x, y - 1] = 1
 		if self.dir == 'RIGHT':
-			pass
+			if y + 1 < self.grid_size:
+				self.grid[x, y + 1] = 1
+		x, y = self.find_tail()
+		self.grid[y, x] = 0
 		
 
+	def is_dead(self):
+		for x in range(self.grid_size):
+			for y in range(self.grid_size):
+				if self.grid[y, x] == 1:
+					return 0
+		return 1
+
+
 	def change_move(self, key):
-		if key == pygame.K_UP:
+		if key == pygame.K_UP and self.dir != 'DOWN':
 			self.dir = 'UP'
-		if key == pygame.K_DOWN:
+		if key == pygame.K_DOWN and self.dir != 'UP':
 			self.dir = 'DOWN'
-		if key == pygame.K_LEFT:
+		if key == pygame.K_LEFT and self.dir != 'RIGHT':
 			self.dir = 'LEFT'
-		if key == pygame.K_RIGHT:
+		if key == pygame.K_RIGHT and self.dir != 'LEFT':
 			self.dir = 'RIGHT'
 
 	def print_grid(self, key):
